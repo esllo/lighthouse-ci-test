d6a6493b296bd9f3a5df63fdf86a277064c89eda
esllo <master@esllo.com>
2022-11-27 00:11:31 +0900

    chore: change loopback to localhost

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: "yarn preview --port 3000",
-      url: ["http://127.0.0.1:3000"],
+      url: ["http://localhost:3000"],
       numberOfRuns: 1,
       startServerReadyPattern: "3000",
       settings: {
